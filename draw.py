9555d0ab005b757be7bbc28bdcfa73d05422f198
유지현 <fbwlgus6@naver.com>
2024-07-30 13:09:40 +0900

    Refactor: convert Korean command to English

--- draw.py
+++ draw.py
@@ -3,58 +3,58 @@ import numpy as np
 import os
 from sklearn.cluster import KMeans
 
-# 사용할 CPU 최대 코어 수 설정
+# Set the maximum number of CPU cores to use
 os.environ['LOKY_MAX_CPU_COUNT'] = '4' 
 
 
 def color_crayon_style(image_path, output_path):
-    # 주어진 경로에서 이미지를 열고 처리
+    # Open images
     with Image.open(image_path) as img:
-        # Step 1: 이미지를 RGB로 변환
+        # Step 1: Convert image to RGB
         img = img.convert("RGB")
         
-        # Step 2: 엣지(가장자리) 검출
+        # Step 2: Detect Edge
         edges = detect_edges(img)
         img = img.filter(ImageFilter.BoxBlur(radius=1))
-        # Step 3: 색상 단순화
+        # Step 3: Simplify colors
         img = simplify_colors(img)
         
-        # Step 4: 엣지와 색상 결합
+        # Step 4: Combine edges and colors
         # combined_image = Image.composite(img, edges, edges.convert("L"))
         combined_image = img
         
-        # 최종 이미지 저장
+        # Save image
         combined_image.save(output_path)
         
-        # 최종 이미지 화면에 표시
+        # Display image
         combined_image.show()
 
 def detect_edges(image):
-    # 이미지 흑백 변환
+    # Convert image to gray scale
     gray_image = image.convert("L")
     
-    # 엣지 검출
+    # Detect Edge
     edges = gray_image.filter(ImageFilter.FIND_EDGES)
     
-    # Binary 이미지로 변환
-    threshold = 30 # 30 이상의 픽셀을 흰색으로 설정
+    # Convert image to binary
+    threshold = 30 # Set 30 or more pixels to white
     binary_edges = edges.point(lambda p: p > threshold and 255)
     
-    # 엣지를 위한 RGB 이미지 생성
+    # Generate RGB image for edges
     edge_color_image = Image.new("RGB", image.size, (255, 255, 255))
-    # 흰색 배경에 검은색 엣지 합성
+    # Composite black edges on white background
     edge_color_image.paste((0, 0, 0), mask=binary_edges)
     
     return edge_color_image
 
 def simplify_colors(image):
-    # 이미지 to numpy 배열 변환
+    # Convert image to numpy array
     img_array = np.array(image)
     
-    # 단순화할 색상의 수
+    # The number of colors to simplify
     num_colors = 16 
     
-    # k-means 클러스터링으로 색상 감소
+    # Reduce colors using k-means clustering
     kmeans = KMeans(n_clusters=num_colors, random_state=0).fit(img_array.reshape(-1, 3))
     
     # 이미지의 각 픽셀을 가장 가까운 클러스터 중심으로 대체
