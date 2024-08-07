7955e9ee2ebbda51c82da8c5d0ce969165b450a2
유지현 <fbwlgus6@naver.com>
2024-08-07 14:45:31 +0900

    Feat: delete duplicate forestRepository

--- supia-back/src/main/java/com/forest/supia/member/service/MemberService.java
+++ supia-back/src/main/java/com/forest/supia/member/service/MemberService.java
@@ -21,8 +21,6 @@ import java.util.List;
 public class MemberService {
     @Autowired
     private MemberRepository memberRepository;
-    @Autowired
-    private ForestRepository forestRepository;
 
     @Autowired
     private ForestRepository forestRepository;
