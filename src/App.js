dc502dd55a70c2f7b908352751a170c980726dfb
seungjunl123 <156068215+seungjunl123@users.noreply.github.com>
2024-08-13 16:36:42 +0900

    openvidutest

--- src/App.js
+++ src/App.js
@@ -125,8 +125,8 @@ class App extends Component {
     this.setState(
       {
         session: this.OV.initSession(),
-        mySessionId: this.state.userId || prevState.mySessionId,
-        myUserName: this.state.memberName || prevState.myUserName,
+        mySessionId: this.state.userId || mySessionId,
+        myUserName: this.state.memberName || myUserName,
       },
       () => {
         var mySession = this.state.session;
