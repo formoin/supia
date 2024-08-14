8c0117e3c7f472e89cd503fceec6cbbf67668611
김민경 <alsrud869@gmail.com>
2024-08-14 21:28:44 +0900

    Fix: modify SSE body to member object"

--- supia-back/src/main/java/com/forest/supia/openvidu/service/OpenviduService.java
+++ supia-back/src/main/java/com/forest/supia/openvidu/service/OpenviduService.java
@@ -2,6 +2,7 @@ package com.forest.supia.openvidu.service;
 
 import com.forest.supia.exception.CustomException;
 import com.forest.supia.exception.ExceptionResponse;
+import com.forest.supia.member.entity.Member;
 import com.forest.supia.member.repository.MemberRepository;
 import com.forest.supia.notification.service.NotificationService;
 import lombok.RequiredArgsConstructor;
@@ -15,7 +16,9 @@ public class OpenviduService {
 
 
     public void sendNotification(long fromMemberId, long toMemberId) {
-        String fromMember = memberRepository.findById(fromMemberId).orElseThrow(()->new ExceptionResponse(CustomException.NOT_FOUND_MEMBER_EXCEPTION)).getName();
+        Member fromMember = memberRepository.findById(fromMemberId).orElseThrow(()->new ExceptionResponse(CustomException.NOT_FOUND_MEMBER_EXCEPTION));
+
+
         notificationService.sendToClient(toMemberId, fromMember, "SSE", "call");
 
     }
