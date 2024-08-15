6c8d6be6f4cc5e8eed2a19adeafd1cce24d7f627
이찬호 <leechanho96@naver.com>
2024-08-16 04:02:48 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -279,6 +279,17 @@ class App extends Component {
         if (newVideoDevice.length > 0) {
           // Creating a new publisher with specific videoSource
           // In mobile devices the default and first camera is the front one
+          let publisher = await this.OV.initPublisherAsync(undefined, {
+                audioSource: undefined, // The source of audio. If undefined default microphone
+                videoSource: undefined, // The source of video. If undefined default webcam
+                publishAudio: true, // Whether you want to start publishing with your audio unmuted or not
+                publishVideo: true, // Whether you want to start publishing with your video enabled or not
+                resolution: "640x480", // The resolution of your video
+                frameRate: 30, // The frame rate of your video
+                insertMode: "APPEND", // How the video is inserted in the target element 'video-container'
+                mirror: false, // Whether to mirror your local video or not
+          });
+
           var currentVideoDeviceId = publisher.stream
                 .getMediaStream()
                 .getVideoTracks()[1]
